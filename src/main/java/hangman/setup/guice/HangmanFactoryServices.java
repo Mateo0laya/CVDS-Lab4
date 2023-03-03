a5d28e4467394ab9885e248bffa8be15db8b43c9
Mateo0laya <mateoolaya20032604@gmail.com>
2023-03-02 20:44:10 -0500

    Implementacion inyección de dependencias

--- src/main/java/hangman/setup/guice/HangmanFactoryServices.java
+++ src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -5,23 +5,41 @@
  */
 package hangman.setup.guice;
 
+
 /**
  *
  * @author 2106913
  */
 
-import hangman.model.French;
 import hangman.model.Language;
+import hangman.model.English;
+import hangman.model.French;
+import hangman.model.Spanish;
+
 import hangman.model.dictionary.HangmanDictionary;
+import hangman.model.dictionary.SpanishDictionaryDataSource;
+import hangman.model.dictionary.EnglishDictionaryDataSource;
 import hangman.model.dictionary.FrenchDictionaryDataSource;
-import hangman.view.HangmanNoviolentoPanel;
+
 import hangman.view.HangmanPanel;
+import hangman.view.HangmanColoridoPanel;
+import hangman.view.HangmanNoviolentoPanel;
+import hangman.view.HangmanStickmanPanel;
+
+import hangman.model.GameScore;
+import hangman.model.BonusScore;
+import hangman.model.OriginalScore;
+import hangman.model.PowerBonusScore;
 
 public class HangmanFactoryServices extends com.google.inject.AbstractModule {
 
     @Override
     protected void configure() {
         /* Guice dependency injection */
+        bind(Language.class).to(Spanish.class);
+        bind(HangmanDictionary.class).to(SpanishDictionaryDataSource.class);
+        bind(HangmanPanel.class).to(HangmanNoviolentoPanel.class);
+        bind(GameScore.class).to(BonusScore.class);
         // bind(Interface.class).to(Concrete.class);
     }
 
